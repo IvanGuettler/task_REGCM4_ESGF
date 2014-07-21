9c77006cffb187d8b8c9afb212ad277a5409861a
regcm <regcm@vihor.gric.dhz.hr>
2014-07-21 08:39:22 +0000

    Moram imati datoteku koja ce lijepo raditi

---
+++ F90_gen_time.f90
@@ -0,0 +1,11 @@
+	program gentime
+		implicit none
+		!Structure
+		! (1) enter start and end of simulation
+		! (2) enter calendar 1 365 leap, 2 365 nonleap, 3 360
+		! (3) compute time steps       for each DM, MM and SM file
+		! (4) compute time step bounds for each DM, MM and SM file
+		! (5) save txt files for othe programs
+
+
+	end program gentime
