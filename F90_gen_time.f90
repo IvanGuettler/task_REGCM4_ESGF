a1500e04e3306d5f8b60be55f04ddaa5a2812e99
regcm <regcm@vihor.gric.dhz.hr>
2014-07-21 08:46:22 +0000

    Radi se. Lijepo je programirati :)

--- F90_gen_time.f90
+++ F90_gen_time.f90
@@ -7,5 +7,23 @@
 		! (4) compute time step bounds for each DM, MM and SM file
 		! (5) save txt files for othe programs
 
+	integer :: Sstart, Send
+	integer :: SstartY, SstartM, SstartD, SendY, SendM, SendD
+
+	print *,"Enter starting date of your simulation (YYYYMMDD):"
+	read *, Sstart
+	print *,"Enter final    date of your simulation (YYYYMMDD):"
+	read *, Send
+	! Compute details
+	SstartY=floor(Sstart/10000)
+	SstartM=floor((Sstart-SstartY)/100)
+	SstartD=Sstart-SstartY-SstartM
+
+	!Check
+	print *,"Year : ",SstartY
+	print *,"Month: ",SstartM
+	print *,"Day  : ",SstartD
+
+
 
 	end program gentime
