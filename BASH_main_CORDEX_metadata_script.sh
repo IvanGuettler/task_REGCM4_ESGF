974db34ff53c7ab68c51f33f58d12122fd7e54cb
regcm <regcm@vihor.gric.dhz.hr>
2014-11-25 08:36:58 +0000

    Radi se

--- BASH_main_CORDEX_metadata_script.sh
+++ BASH_main_CORDEX_metadata_script.sh
@@ -23,10 +23,10 @@ NCO_PATH='/home1/regcm/regcmlibs_my_nco/bin'
 #--> Select activities
        INDX=1 #WHICH VARIABLE? (use CORDEX_metadata_common to read more).
     collect=0 #Collect variable from various sources        
-      means=0 #Calculate daily, monthly and seasonal means  
-  rm_buffer=0 #Remove buffer zone e.g. 11 grid cells        
-interpolate=0 #Interpolate to regular CORDEX grid (0.5 or 0.125 deg)
-      split=0 #Split files into specific groups             
+      means=1 #Calculate daily, monthly and seasonal means  
+  rm_buffer=1 #Remove buffer zone e.g. 11 grid cells        
+interpolate=1 #Interpolate to regular CORDEX grid (0.5 or 0.125 deg)
+      split=1 #Split files into specific groups             
    metadata=1 #Edit meta-data                              
     convert=0 #Convert from netcdf3 > netcdf4 if needed
 
@@ -361,8 +361,8 @@ if [ ${metadata} == 1 ] ; then
     FILENUMBER=$((FILENUMBER-1))
 
     file=0
-    while [ ${file} -le ${FILENUMBER} ] ; do
-#   while [ ${file} -le 0 ] ; do
+     while [ ${file} -le ${FILENUMBER} ] ; do
+#    while [ ${file} -le 0 ] ; do
           echo ${EDITIN[${file}]}
        EDITING=${EDITIN[${file}]} #This is the file we will work on
 
@@ -376,7 +376,7 @@ if [ ${metadata} == 1 ] ; then
             INTERPI=1     #--------------------------> Interpolated grid
        fi
     #---
-    #PREPHASE 2: determine grid type from the filaname day mon sem
+    #PREPHASE 2: determine grid type from the filename day mon sem
     #---
        if  grep -q "_day_"  <<< "${EDITING}"  ; then
             FREQ='day'
@@ -388,14 +388,14 @@ if [ ${metadata} == 1 ] ; then
             FREQ='sem'
        fi
 
-
+echo "-----------------------------------------------------------------------------1"
     #---
     #PHASE 1: remove all global&local atributes
     #---
     ${NCO_PATH}/ncatted -O -h -a ,global,d,,     $EDITING
     ${NCO_PATH}/ncatted -O -h -a ,,d,,           $EDITING
 
-
+echo "-----------------------------------------------------------------------------2"
     #---
     #PHASE 2: add global atributes needed by CORDEX
     #---
@@ -415,6 +415,7 @@ if [ ${metadata} == 1 ] ; then
                               -a CORDEX_domain,global,c,c,"${CORDEX_domain}"                                   \
                               -a product,global,c,c,"${product}"                                               \
                               -a references,global,c,c,"${references}" ${EDITING}
+echo "-----------------------------------------------------------------------------3"
     #---
     #PHASE 3: rename dimensions
     #---
@@ -433,6 +434,7 @@ if [ ${metadata} == 1 ] ; then
                                -d m2,lev ${EDITING}
     fi
 
+echo "-----------------------------------------------------------------------------4"
     #---
     #PHASE 4: edit local attributes of variables and variable-dimnesion
     #---
@@ -452,40 +454,57 @@ if [ ${metadata} == 1 ] ; then
 	${NCO_PATH}/ncatted -O -h -a     cell_methods,${name[${INDX}]},c,c,"${cellMethodMMSM[${INDX}]}"  ${EDITING}
      fi
 
-#
-#    ${NCO_PATH}/ncap -O -h -s "height=double(2);lon=double(lon);lat=double(lat)"  $EDITING test.nc
-#    mv test.nc $EDITING
-#
-#    ${NCO_PATH}/ncatted -O -h -a long_name,height,c,c,${H2_longname} \
-#                              -a standard_name,height,c,c,${H2_standardname} \
-#                              -a units,height,c,c,${H2_units} \
-#                              -a axis,height,c,c,${H2_axis} \
-#                              -a positive,height,c,c,${H2_positive} \
-#                              -a axis,lon,c,c,"X" \
-#                              -a axis,lat,c,c,"Y" \
-#                              -a units,lon,c,c,"degrees_east" \
-#                              -a units,lat,c,c,"degrees_north"  \
-#                              -a long_name,lon,c,c,"longitude" \
-#                              -a long_name,lat,c,c,"latitude"  \
-#                              -a standard_name,lon,c,c,"longitude" \
-#                              -a standard_name,lat,c,c,"latitude" $EDITING
+echo "-----------------------------------------------------------------------------5"
+    #---
+    #PHASE 5: edit height (this has to be generalized)
+    #---
 
-    #---
-    #PHASE 5: edit projection related stuff
-    #---
-#    if [ ${INTERPI} == 0 ]; then
-#    ${NCO_PATH}/ncatted -O -h -a  grid_mapping,${name[${INDEX}]},c,c,"${projection_name}" $EDITING
-#    ${NCO_PATH}/ncks -A -h -v ${projection_name} map.nc $EDITING
-#    fi
+    ${NCO_PATH}/ncrename -O -h -v m2,height                                           ${EDITING}
+    ${NCO_PATH}/ncap2    -O -h -s "height=double(2);lon=double(lon);lat=double(lat)"  ${EDITING} ${tempTarget}/test.nc
+    mv ${tempTarget}/test.nc ${EDITING}
 
-    #---
-    #PHASE 5: edit time related stuff
-    #---
-#    ${NCO_PATH}/ncatted -O -h -a long_name,time,c,c,${time_long_name} \
-#                              -a standard_name,time,c,c,${time_standard_name}  \
-#                              -a axis,time,c,c,${time_axis} $EDITING
+    ${NCO_PATH}/ncatted -O -h -a long_name,height,c,c,${H2_longname}         \
+                              -a standard_name,height,c,c,${H2_standardname} \
+                              -a units,height,c,c,${H2_units}                \
+                              -a axis,height,c,c,${H2_axis}                  \
+                              -a positive,height,c,c,${H2_positive}          ${EDITING}
 
 
+echo "-----------------------------------------------------------------------------6"
+    #---
+    #PHASE 6: edit projection related stuff
+    #---
+    ${NCO_PATH}/ncatted -O -h -a axis,lon,c,c,"X"                  \
+                              -a axis,lat,c,c,"Y"                  \
+                              -a units,lon,c,c,"degrees_east"      \
+                              -a units,lat,c,c,"degrees_north"     \
+                              -a long_name,lon,c,c,"longitude"     \
+                              -a long_name,lat,c,c,"latitude"      \
+                              -a standard_name,lon,c,c,"longitude" \
+                              -a standard_name,lat,c,c,"latitude" ${EDITING}
+    if [ ${INTERPI} == 0 ]; then
+           ${NCO_PATH}/ncatted -O -h -a  grid_mapping,${name[${INDX}]},c,c,"${projection_name}" ${EDITING}
+           ${NCO_PATH}/ncap2   -O -h -s  'Lambert_Conformal="Lambert_Conformal"'                ${EDITING}
+           ${NCO_PATH}/ncatted -O -h -a grid_mapping_name,Lambert_Conformal,c,c,${projection_grid_mapping_name}                          \
+                                     -a standard_parallel,Lambert_Conformal,c,f,${projection_standard_parallel1}                         \
+                                     -a standard_parallel,Lambert_Conformal,a,f,${projection_standard_parallel2}                         \
+                                     -a longitude_of_central_meridian,Lambert_Conformal,c,f,${projection_longitude_of_central_meridian}  \
+                                     -a latitude_of_projection_origin,Lambert_Conformal,c,f,${projection_latitude_of_projection_origin}  ${EDITING}
+
+    fi
+
+echo "-----------------------------------------------------------------------------7"
+  
+    #---
+    #PHASE 7: edit time related stuff
+    #---
+    ${NCO_PATH}/ncatted -O -h -a long_name,time,c,c,${time_long_name}           \
+                              -a standard_name,time,c,c,${time_standard_name}   \
+                              -a axis,time,c,c,${time_axis}                     \
+                              -a units,time,c,c,"days since 1949-12-01T00:00:00Z"                   \
+                              -a calendar,time,c,c,${time_calendar}             \
+                              -a time_bounds,time,c,c,${time_bounds}            ${EDITING}
+    
     file=$((file+1))
     done
 fi
